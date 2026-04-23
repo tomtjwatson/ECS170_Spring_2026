3a9f1a5ce0f84d707512fd3bcd4e9daa72087c56
Annie <annzuo@ucdavis.edu>
2026-04-23 14:11:23 -0700

    mlp code

--- local_code/stage_2_code/Method_MLP.py
+++ local_code/stage_2_code/Method_MLP.py
@@ -10,12 +10,13 @@ from local_code.stage_2_code.Evaluate_Accuracy import Evaluate_Accuracy
 import torch
 from torch import nn
 import numpy as np
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
 
 
 class Method_MLP(method, nn.Module):
     data = None
     # it defines the max rounds to train the model
-    max_epoch = 100
+    max_epoch = 500
     # it defines the learning rate for gradient descent based optimizer for model learning
     learning_rate = 1e-3
 
@@ -27,11 +28,15 @@ class Method_MLP(method, nn.Module):
         nn.Module.__init__(self)
         # check here for nn.Linear doc: https://pytorch.org/docs/stable/generated/torch.nn.Linear.html
         self.fc_layer_1 = nn.Linear(784, 256)
+        self.bn_1 = nn.BatchNorm1d(256) 
         # check here for nn.ReLU doc: https://pytorch.org/docs/stable/generated/torch.nn.ReLU.html
         self.activation_func_1 = nn.ReLU()
-        self.fc_layer_2 = nn.Linear(256, 10)
+        self.fc_layer_2 = nn.Linear(256, 128)
+        self.bn_2 = nn.BatchNorm1d(128) 
+        self.activation_func_2 = nn.ReLU()
         # check here for nn.Softmax doc: https://pytorch.org/docs/stable/generated/torch.nn.Softmax.html
-        self.activation_func_2 = nn.Softmax(dim=1)
+        self.fc_layer_3 = nn.Linear(128, 10)
+        self.activation_func_3 = nn.Softmax(dim=1)
 
     # it defines the forward propagation function for input x
     # this function will calculate the output layer by layer
@@ -39,12 +44,13 @@ class Method_MLP(method, nn.Module):
     def forward(self, x):
         '''Forward propagation'''
         # hidden layer embeddings
-        h = self.activation_func_1(self.fc_layer_1(x))
+        h = self.activation_func_1(self.bn_1(self.fc_layer_1(x)))
         # outout layer result
         # self.fc_layer_2(h) will be a nx2 tensor
         # n (denotes the input instance number): 0th dimension; 2 (denotes the class number): 1st dimension
         # we do softmax along dim=1 to get the normalized classification probability distributions for each instance
-        y_pred = self.activation_func_2(self.fc_layer_2(h))
+        h = self.activation_func_2(self.bn_2(self.fc_layer_2(h)))
+        y_pred = self.activation_func_3(self.fc_layer_3(h))
         return y_pred
 
     # backward error propagation will be implemented by pytorch automatically
@@ -78,15 +84,9 @@ class Method_MLP(method, nn.Module):
             # update the variables according to the optimizer and the gradients calculated by the above loss.backward function
             optimizer.step()
 
-            if epoch%10 == 0:
+            if epoch%100 == 0:
                 accuracy_evaluator.data = {'true_y': y_true, 'pred_y': y_pred.max(1)[1]}
-                metrics = accuracy_evaluator.evaluate()
-                print('Epoch:', epoch,
-                      'Accuracy:', metrics['Accuracy'],
-                      'Loss:', train_loss.item(),
-                      'F1:', metrics['F1'],
-                      'Precision:', metrics['Precision'],
-                      'Recall:', metrics['Recall'])
+                print('Epoch:', epoch, 'Accuracy:', accuracy_evaluator.evaluate(), 'Loss:', train_loss.item())
     
     def test(self, X):
         # do the testing, and result the result
@@ -95,11 +95,52 @@ class Method_MLP(method, nn.Module):
         # instances will get the labels corresponding to the largest probability
         return y_pred.max(1)[1]
     
+    def evaluate_metrics(self, true_y, pred_y):
+        accuracy  = accuracy_score(true_y, pred_y)
+
+        f1_weighted = f1_score(true_y, pred_y, average='weighted')
+        f1_macro = f1_score(true_y, pred_y, average='macro')
+        f1_micro = f1_score(true_y, pred_y, average='micro')
+
+        recall_weighted = recall_score(true_y, pred_y, average='weighted')
+        recall_macro = recall_score(true_y, pred_y, average='macro')
+        recall_micro = recall_score(true_y, pred_y, average='micro')
+
+        precision_weighted = precision_score(true_y, pred_y, average='weighted')
+        precision_macro = precision_score(true_y, pred_y, average='macro')
+        precision_micro = precision_score(true_y, pred_y, average='micro')
+
+        print(f'  Accuracy:            {accuracy:.4f}')
+        print(f'  F1(weighted):       {f1_weighted:.4f}')
+        print(f'  F1(macro):          {f1_macro:.4f}')
+        print(f'  F1(micro):          {f1_micro:.4f}')
+        print(f'  Recall(weighted):   {recall_weighted:.4f}')
+        print(f'  Recall(macro):      {recall_macro:.4f}')
+        print(f'  Recall(micro):      {recall_micro:.4f}')
+        print(f'  Precision(weighted):{precision_weighted:.4f}')
+        print(f'  Precision(macro):   {precision_macro:.4f}')
+        print(f'  Precision(micro):   {precision_micro:.4f}')
+
+        return {
+            'accuracy':           accuracy,
+            'f1_weighted':        f1_weighted,
+            'f1_macro':           f1_macro,
+            'f1_micro':           f1_micro,
+            'recall_weighted':    recall_weighted,
+            'recall_macro':       recall_macro,
+            'recall_micro':       recall_micro,
+            'precision_weighted': precision_weighted,
+            'precision_macro':    precision_macro,
+            'precision_micro':    precision_micro,
+        }
+    
     def run(self):
         print('method running...')
         print('--start training...')
         self.train(self.data['train']['X'], self.data['train']['y'])
         print('--start testing...')
         pred_y = self.test(self.data['test']['X'])
-        return {'pred_y': pred_y, 'true_y': self.data['test']['y']}
+        true_y = self.data['test']['y']
+        metrics = self.evaluate_metrics(true_y, pred_y)
+        return {'pred_y': pred_y, 'true_y': self.data['test']['y'], 'metrics': metrics}
             
