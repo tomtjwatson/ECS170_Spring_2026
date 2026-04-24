fed2e3425de0abf1cd782f2a99b59c92a6dd3a0b
Annie <annzuo@ucdavis.edu>
2026-04-23 18:02:02 -0700

    fix accuracy output

--- local_code/stage_2_code/Evaluate_Accuracy.py
+++ local_code/stage_2_code/Evaluate_Accuracy.py
@@ -8,18 +8,26 @@ Concrete Evaluate class for a specific evaluation metrics
 from local_code.base_class.evaluate import evaluate
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
+
 class Evaluate_Accuracy(evaluate):
     data = None
     
     def evaluate(self):
-        print('evaluating performance...')
+        # print('evaluating performance...')
         # return accuracy_score(self.data['true_y'], self.data['pred_y'])
         true_y = self.data['true_y']
         pred_y = self.data['pred_y']
         return {
             'Accuracy': accuracy_score(true_y, pred_y),
-            'F1': f1_score(true_y, pred_y, average='weighted', zero_division=0),
-            'Precision': precision_score(true_y, pred_y, average='weighted', zero_division=0),
-            'Recall': recall_score(true_y, pred_y, average='weighted', zero_division=0),
+            'F1 weighted': f1_score(true_y, pred_y, average='weighted', zero_division=0),
+            'F1 macro' : f1_score(true_y, pred_y, average='macro'),
+            'F1 micro' : f1_score(true_y, pred_y, average='micro'),
+            'Precision weighted': precision_score(true_y, pred_y, average='weighted', zero_division=0),
+            'Precision macro': precision_score(true_y, pred_y, average='macro'),
+            'Precision micro': precision_score(true_y, pred_y, average='micro'),
+            'Recall weighted': recall_score(true_y, pred_y, average='weighted', zero_division=0),
+            'Recall macro': recall_score(true_y, pred_y, average='macro'),
+            'Recall micro': recall_score(true_y, pred_y, average='micro')
         }
+
         
